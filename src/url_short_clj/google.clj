8dad007a923e699e04886adbedb23a6118b15135
Ivan Ivic <tyrion85@gmail.com>
2014-10-04 17:32:41 +0200

    Added short URL expand via Google URL Shortener Service

--- src/url_short_clj/google.clj
+++ src/url_short_clj/google.clj
@@ -2,24 +2,29 @@
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]))
 
-(defn- call-google-api [url apiKey]
+(defn- shorten-via-google-api [url apiKey]
   (client/post
    (str "https://www.googleapis.com/urlshortener/v1/url?key=" apiKey)
    {:content-type :json
     :body (str "{\"longUrl\":\"" url "\"}")}))
+
+(defn- expand-via-google-api [url]
+  (client/get
+   (str "https://www.googleapis.com/urlshortener/v1/url?shortUrl=" url)))
 
 (defn- parse-response-body [response-body]
   (json/read-str
    response-body
    :key-fn keyword))
 
-(defn shorten-url [url apiKey]
-  (let [response (call-google-api url apiKey)
+(defn shorten [url apiKey]
+  (let [response (shorten-via-google-api url apiKey)
         response-body (parse-response-body (:body response))]
-   (hash-map :body response-body
-             :status (:status response)
-             :kind (:kind response-body)
-             :id (:id response-body)
-             :long-url (:longUrl response-body))))
+   (:id response-body)))
+
+(defn expand [url]
+  (let [response (expand-via-google-api url)
+        response-body (parse-response-body (:body response))]
+    (:longUrl response-body)))
 
 
--- test/url_short_clj/google_test.clj
+++ test/url_short_clj/google_test.clj
@@ -1,8 +1,12 @@
 (ns url-short-clj.google-test
   (:use midje.sweet url-short-clj.google))
 
-(fact "Long url of shortened http://www.google.com should remain the same after shorening"
-      (:long-url (shorten-url
+(fact "Short url of shortened 'http://www.google.com' for api key '' should be 'http://goo.gl/fbsS'"
+      (shorten
        "http://www.google.com"
        "AIzaSyA0YzaajSNy2r6Tp8jaXzQOLp1-B1uWsk4")
-      ) => "http://www.google.com/")
+      => "http://goo.gl/fbsS")
+
+(fact "Lengthening a short url 'http://goo.gl/fbsS' should produce 'http://www.google.com/'"
+      (expand "http://goo.gl/fbsS")
+      => "http://www.google.com/")
